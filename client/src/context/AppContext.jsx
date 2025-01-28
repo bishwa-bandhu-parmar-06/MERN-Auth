e210f1d131bf48a545a21fafe8b6767e15107723
BISHWA BANDHU PARMAR <bishwabandhuparmar06@gmail.com>
2025-01-28 15:35:36 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider = ({ children }) => {
 
   axios.defaults.withCredentials = true;
 
-  const backendUri = import.meta.env.VITE_BACKEND_URL; // Ensure this is set in your environment variables
+  const backendUri = "https://mern-auth-p7ma.onrender.com"; // Ensure this is set in your environment variables
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(null); // Use null instead of false for better semantic
 
